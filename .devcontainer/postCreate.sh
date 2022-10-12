9907a2752f2f0951a73d5dd233812129f1c1ad71
Jason Vercellone <vercellone@users.noreply.github.com>
2022-10-12 21:47:45 +0000

    Microsoft.dotnet-interactive_profile.ps1

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,3 +4,13 @@ foreach($module in 'Az','Pester') {
     Install-Module -Name $module -Scope AllUsers -Repository PSGallery
 }
 Set-PSRepository -Name PSGallery -InstallationPolicy Untrusted
+
+# Use the interactive profile to Import Modules for notebooks
+$interactiveProfileParams = @{
+    Path  = Join-Path -Path $env:HOME -ChildPath '.config/powershell/Microsoft.dotnet-interactive_profile.ps1'
+    Value = 'Get-ChildItem -Path /workspaces/my-first-codespace/.devcontainer/Modules -Filter *.psd1 -Recurse | Import-Module'
+}
+if (! (Test-Path $interactiveProfileParams.Path)) {
+    New-Item -Path $interactiveProfileParams.Path -Force -Type File
+}
+$null = Set-Content @interactiveProfileParams
