f1f8521b13d3bd915e1f7cc65f92af48d851247d
Jason Vercellone <vercellone@users.noreply.github.com>
2022-10-14 18:41:56 +0000

    Do not import Az* modules for notebook profile

--- .devcontainer/postCreate.ps1
+++ .devcontainer/postCreate.ps1
@@ -2,7 +2,10 @@
 # Use the interactive profile to Import Modules for notebooks
 $interactiveProfileParams = @{
     Path  = Join-Path -Path $env:HOME -ChildPath '.config/powershell/Microsoft.dotnet-interactive_profile.ps1'
-    Value = 'Get-ChildItem -Path /workspaces/my-first-codespace/.devcontainer/Modules -Filter *.psd1 -Recurse | Import-Module'
+    # Import all embedded modules
+    # Value = 'Get-ChildItem -Path /workspaces/my-first-codespace/.devcontainer/Modules -Filter *.psd1 -Recurse | Import-Module'
+    # Import only DataAdapter
+    Value = 'Import-Module /workspaces/my-first-codespace/.devcontainer/Modules/DataAdapter/0.9.21/DataAdapter.psd1'
 }
 if (! (Test-Path $interactiveProfileParams.Path)) {
     New-Item -Path $interactiveProfileParams.Path -Force -Type File
